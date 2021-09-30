7fdabb6ab6704aefacef11ebae73c7ba3a325d8b
bhodzic1 <bhodzic1@etf.unsa.ba>
2021-09-30 09:28:57 +0200

    Some changes made

--- azil/src/components/navbar/NavigationBar.js
+++ azil/src/components/navbar/NavigationBar.js
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { AzilContext } from '../../context/AzilContext';
 
 const NavigationBar = () => {
-
+  const { role, loggedIn } = useContext(AzilContext);
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-    
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
+      <Container>
+      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end"> 
+        <Nav>
+          <Nav.Link href="/adopts">My adopts</Nav.Link>
+          <Nav.Link href="/profile">Profile</Nav.Link>
+
+          { <Nav.Link href="/login">Login</Nav.Link> }
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
 </Navbar>
   )
 }
